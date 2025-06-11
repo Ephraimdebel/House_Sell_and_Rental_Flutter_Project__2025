dc0a560d2432d4f63774acf85cc93c2f4c20fd3b
srtt98 <sarontadesse98@gmail.com>
2025-06-10 19:11:10 -0700

    ui of home page added by saron

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'src/features/home/presentation/pages/home_page.dart';
+// import 'src/features/home/presentation/pages/home_page.dart';
 import 'src/features/login/presentation/pages/login_page.dart';
-import 'src/features/signup/presentation/pages/signup_page.dart';
+// import 'src/features/signup/presentation/pages/signup_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -13,16 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'House Listing App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
-    );
     // return MaterialApp(
     //   title: 'House Listing App',
     //   theme: ThemeData(primarySwatch: Colors.blue),
-    //   home: LoginPage(),
+    //   home: const HomePage(),
     // );
+    return MaterialApp(
+      title: 'House Listing App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
+    );
     // return MaterialApp(
     //   title: 'House Listing App',
     //   theme: ThemeData(primarySwatch: Colors.blue),
