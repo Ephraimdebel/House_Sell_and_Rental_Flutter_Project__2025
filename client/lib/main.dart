bd8407b257df393be160ea1100cdab5348b67111
srtt98 <sarontadesse98@gmail.com>
2025-06-10 23:23:34 -0700

    home page presentation added

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'src/features/home/presentation/pages/home_page.dart';
 import 'src/features/login/presentation/pages/login_page.dart';
-// import 'src/features/signup/presentation/pages/signup_page.dart';
+import 'src/features/signup/presentation/pages/signup_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -18,15 +18,15 @@ class MyApp extends StatelessWidget {
     //   theme: ThemeData(primarySwatch: Colors.blue),
     //   home: const HomePage(),
     // );
-    return MaterialApp(
-      title: 'House Listing App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
-    );
     // return MaterialApp(
     //   title: 'House Listing App',
     //   theme: ThemeData(primarySwatch: Colors.blue),
-    //   home: SignUpPage(),
+    //   home: LoginPage(),
     // );
+    return MaterialApp(
+      title: 'House Listing App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SignUpPage(),
+    );
   }
 }
